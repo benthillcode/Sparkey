4370ddd1f3710af1fa6f76b3c39e1824101c9d03
coolcode <148582252+benthillcode@users.noreply.github.com>
2025-07-02 00:12:34 -0400

    Delete app.js

--- app.js
+++
@@ -1,7 +0,0 @@
-function generateColors() {
-  const palette = chroma.scale(['#f72585', '#7209b7', '#3a0ca3', '#4361ee', '#4cc9f0']).mode('lch').colors(3);
-
-  document.getElementById("body").setAttribute("fill", palette[0]);
-  document.getElementById("button").setAttribute("fill", palette[1]);
-  document.getElementById("flame").setAttribute("fill", palette[2]);
-}
